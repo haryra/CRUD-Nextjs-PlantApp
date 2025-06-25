5ffe60aa3f5dc63aadc0cd880ab62c864a5e786d
haryra <haryraachmat10@gmail.com>
2025-06-25 11:33:35 +0700

    Add Login and Logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
-import { House, Sprout } from "lucide-react"
+import { House, LogIn, LogOut, Sprout } from "lucide-react"
 import { ModeToggle } from "./ModeToggle"
+import { stackServerApp } from "@/stack"
+import { getUserDetails } from "@/actions/user.action"
+import { UserButton } from "@stackframe/stack"
 
-function Navbar() {
+async function Navbar() {
+    const user = await stackServerApp.getUser();
+    const app = stackServerApp.urls;
+    const userProfile = await getUserDetails(user?.id);
+
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
         <div className="max-w-7xl mx-auto px-4 ">
@@ -30,6 +37,31 @@ function Navbar() {
                 </Button>
 
                 <ModeToggle />
+
+                {user ? (<>
+                {/* Log Out Button */}
+                <Button variant="ghost" className="flex items-center gap-2" asChild>
+                    <Link href={app.signIn}>
+                        <LogOut className="w-4 h-4"/>
+                        <span className="hidden lg:inline">Log Out</span>
+                    </Link>
+                </Button>
+
+                <UserButton />
+                </>) : (<>
+                {/* Sign In Button */}
+                <Button variant="ghost" className="flex items-center gap-2" asChild>
+                    <Link href={app.signIn}>
+                        <LogIn className="w-4 h-4" />
+                        <span className="hidden lg:inline">Sign In</span>
+                    </Link>
+                </Button>
+                </>)}
+
+                
+
+                
+
                 </div>
             </div>
         </div>
