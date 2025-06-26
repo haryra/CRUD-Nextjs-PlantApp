e7a00556f5d4c7f6bf2b05ff67017ff5b3f25f6a
haryra <haryraachmat10@gmail.com>
2025-06-26 12:04:08 +0700

    Integration Prisma Database

--- src/app/plants/page.tsx
+++ src/app/plants/page.tsx
@@ -10,7 +10,11 @@ async function page() {
   return (
     <>
     {user ? (
-        <InventoryTable />
+        <div className='mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6'>
+          <div className='lg:col-span-full'>
+          <InventoryTable />
+          </div>
+        </div>
     ) : (
         <div className="flex justify-center mt-20 items-center">
             <SignUp />
--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import {
   Table,
   TableBody,
@@ -9,8 +11,14 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { Search } from 'lucide-react';
+import { Combobox } from './ui/combo-box';
 
 function InventoryTable() {
+    const [selectedCategory,setselectedCategory] = useState("");
+
     const plants = [
   {
     id: "inv001",
@@ -24,7 +32,15 @@ function InventoryTable() {
         <div className='flex items-center gap-2 py-4'>
             <div className='relative max-w-sm w-full'>
                 {/* Add Input and Search */}
+                {/* Input */}
+                    <Input
+                      placeholder="Filter Plant"
+                      className="pl-10 rounded-lg focus-visible:ring-0"
+                    />
+                    <Search className='absolute h-4 w-4 left-3 top-1/2 transform -translate-y-1/2'/>
+                {/* end Input */}
             </div>
+            <Combobox value={selectedCategory} onChange={(val) => setselectedCategory(val)} />
         </div>
 
         <Table>
@@ -35,7 +51,7 @@ function InventoryTable() {
           <TableHead>Category</TableHead>
           <TableHead>Price</TableHead>
           <TableHead>Stock</TableHead>
-          <TableHead>Action</TableHead>
+          <TableHead className="text-right">Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
