fab20112c71e57ee8d08a5f6e74ee1018082eda3
haryra <haryraachmat10@gmail.com>
2025-06-30 15:12:46 +0700

    fetch data from database

--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -15,11 +15,11 @@ import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Search } from 'lucide-react';
 import { Combobox } from './ui/combo-box';
+import { getPlants } from '@/actions/plant.action';
+import { useRouter } from 'next/navigation';
 
-function InventoryTable() {
-    const [selectedCategory,setselectedCategory] = useState("");
 
-    const plants = [
+const plants = [
   {
     id: "inv001",
     name: "plant 1",
@@ -27,6 +27,26 @@ function InventoryTable() {
     price: 250,
     stock: 5,
   }];
+
+  type Plants = Awaited<ReturnType<typeof getPlants>>;
+
+  interface InventoryTableProps {
+  plants: Plants;
+}
+
+
+function InventoryTable({plants}: InventoryTableProps) {
+    const router = useRouter();
+    const [selectedCategory,setselectedCategory] = useState("");
+     const [searchTerm, setSearchTerm] = useState("");
+
+    //  Filter plants by name and category (if selected)
+    const filteredPlants = plants?.userPlants?.filter(
+      (plant) => 
+        plant.name.toLowerCase().includes(searchTerm.toLowerCase()) && (selectedCategory == "" || plant.category === selectedCategory)
+    );
+
+
   return (
     <div className='w-full'>
         <div className='flex items-center gap-2 py-4'>
@@ -36,6 +56,8 @@ function InventoryTable() {
                     <Input
                       placeholder="Filter Plant"
                       className="pl-10 rounded-lg focus-visible:ring-0"
+                      value={searchTerm}
+                      onChange={(e) => setSearchTerm(e.target.value)}
                     />
                     <Search className='absolute h-4 w-4 left-3 top-1/2 transform -translate-y-1/2'/>
                 {/* end Input */}
@@ -55,8 +77,13 @@ function InventoryTable() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {plants.map((plant) => (
-          <TableRow key={plant.id}>
+        {filteredPlants?.map((plant) => {
+         const slugifiedName = plant.name.toLowerCase().replace(/\s+/g, "-");
+        const slug = `${plant.id}--${slugifiedName}`;
+        const plantUrl = `/plants/${slug}`;
+         return(
+
+          <TableRow key={plant.id} onClick={() => router.push(plantUrl)}>
             <TableCell>{plant.id}</TableCell>
             <TableCell>{plant.name}</TableCell>
             <TableCell>{plant.category}</TableCell>
@@ -69,7 +96,7 @@ function InventoryTable() {
                 </div>
             </TableCell>
           </TableRow>
-        ))}
+        )})}
       </TableBody>
       
     </Table>
